a2281ca17198496f65f1c2af5f08b52e1c5d83a2
TeetyK <teetysupergame7484@gmail.com>
2026-02-16 19:20:08 +0700

    fix: api routes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = 8000;
 
-const endpointSecret = "xxxx"; // เอาได้จากเว็บของ Stripe
+const endpointSecret = "https://buy.stripe.com/test_6oU00bfJJ7llchafxCao800"; // เอาได้จากเว็บของ Stripe
 
 // Middlewares here
 app.use(cors());
@@ -25,7 +25,108 @@ const initMySQL = async () => {
     database: "tutorial",
   });
 };
+app.post("/api/checkout", express.json(), async (req, res) => {
+  const { product, information } = req.body;
+  try {
+    // create payment session
+    const orderId = uuidv4();
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "thb",
+            product_data: {
+              name: product.name,
+            },
+            unit_amount: product.price * 100,
+          },
+          quantity: product.quantity,
+        },
+      ],
+      mode: "payment",
+      success_url: `http://localhost:8888/success.html?id=${orderId}`,
+      cancel_url: `http://localhost:8888/cancel.html?id=${orderId}`,
+    });
 
+    // create order in database (name, address, session id, status)
+    console.log("session", session);
+
+    const data = {
+      name: information.name,
+      address: information.address,
+      session_id: session.id,
+      status: session.status,
+      order_id: orderId,
+    };
+
+    const [result] = await conn.query("INSERT INTO orders SET ?", data);
+
+    res.json({
+      message: "Checkout success.",
+      id: session.id,
+      result,
+    });
+  } catch (error) {
+    console.error("Error creating user:", error.message);
+    res.status(400).json({ error: "Error payment" });
+  }
+});
+app.post("/webhook", express.raw({ type: "application/json" }), async (req, res) => {
+  const sig = req.headers["stripe-signature"];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+  } catch (err) {
+    res.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case "checkout.session.completed":
+      const paymentSuccessData = event.data.object;
+      const sessionId = paymentSuccessData.id;
+
+      const data = {
+        status: paymentSuccessData.status,
+      };
+
+      const result = await conn.query("UPDATE orders SET ? WHERE session_id = ?", [
+        data,
+        sessionId,
+      ]);
+
+      console.log("=== update result", result);
+
+      // event.data.object.id = session.id
+      // event.data.object.customer_details คือข้อมูลลูกค้า
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  res.send();
+});
+app.get("/order/:id", async (req, res) => {
+  const orderId = req.params.id;
+  try {
+    const [result] = await conn.query("SELECT * from orders where order_id = ?", orderId);
+    const selectedOrder = result[0];
+    if (!selectedOrder) {
+      throw {
+        errorMessage: "Order not found",
+      };
+    }
+    res.json(selectedOrder);
+  } catch (error) {
+    console.log("error", error);
+    res.status(404).json({ error: error.errorMessage || "System error" });
+  }
+});
 
 // Listen
 app.listen(port, async () => {
